5b7b3868ba61141ffd2e47ad6231d4b798b16cb9
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-06-27 16:33:32 -0600

    test

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -4,8 +4,8 @@ import SkillList from "../../common/SkillList";
 
 function Skills() {
   return (
-    <section id="skills" className={StyleSheet.container}>
-      <h1>Skills</h1>
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="HTML"/>
         <SkillList src={checkMarkIcon} skill="CSS"/>
