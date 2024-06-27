7b3856186026fe285c56219bea59b63159e142e1
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-06-27 16:18:36 -0600

    test

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,16 +1,39 @@
 import styles from "./ProjectsStyles.module.css";
 import viber from "../../assets/viberr.png";
+import freshBurger from "../../assets/fresh-burger.png"
+import hipsster from "../../assets/hipsster.png"
+import fitLift from "../../assets/fitlift.png"
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a href="https://mahajabins-todolist.netlify.app/" target="_blank">
-          <img className="hover" src={viber} alt="Viber logo" />
-          <h3>Retro Todos</h3>
-          <p>Todo List App</p>
-        </a>
+        <ProjectCard
+          src={viber}
+          link="https://mahajabins-todolist.netlify.app/"
+          h3="Retro Todos"
+          p="Todo List App in a Retro style"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://mahajabins-todolist.netlify.app/"
+          h3="Title"
+          p="Lorem ipsum dolor sit amet."
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://mahajabins-todolist.netlify.app/"
+          h3="Title"
+          p="Lorem ipsum dolor sit amet."
+        />
+        <ProjectCard
+          src={fitLift}
+          link="https://mahajabins-todolist.netlify.app/"
+          h3="Title"
+          p="Lorem ipsum dolor sit amet."
+        />
       </div>
     </section>
   );
