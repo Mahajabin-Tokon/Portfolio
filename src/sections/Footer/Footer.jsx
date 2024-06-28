c563f7370c99fe54df683f751897f6677bf04844
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-06-27 19:46:01 -0600

    test

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from "./FooterStyles.module.css";
+
+function Footer() {
+  return (
+    <section id="footer" className={styles.container}>
+      <p>
+        &#169; 2024 Mahajabin Tokon. <br />
+        All rights reserved.
+      </p>
+    </section>
+  );
+}
+
+export default Footer;
