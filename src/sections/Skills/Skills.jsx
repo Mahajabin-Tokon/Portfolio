ba201a492525ed3189fb3703aec71a7cdd721f19
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-06-28 20:43:32 -0600

    test

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -12,27 +12,35 @@ function Skills() {
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
+      <h2>Expertise</h2>
+      <br />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="HTML"/>
         <SkillList src={checkMarkIcon} skill="CSS"/>
         <SkillList src={checkMarkIcon} skill="JavaScript"/>
-        <SkillList src={checkMarkIcon} skill="TypeScript"/>
-        <SkillList src={checkMarkIcon} skill="Node"/>
-      </div>
-      <hr />
-      <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="React"/>
-        <SkillList src={checkMarkIcon} skill="Angular"/>
-        <SkillList src={checkMarkIcon} skill="Vue"/>
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS"/>
+        <SkillList src={checkMarkIcon} skill="Python"/>
       </div>
       <hr />
+      <h2>Familiar</h2>
+      <br />
       <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Node.js"/>
+        <SkillList src={checkMarkIcon} skill="Express.js"/>
+        <SkillList src={checkMarkIcon} skill="MongoDB"/>
+        <SkillList src={checkMarkIcon} skill="Java"/>
+      </div>
+      <hr />
+      <h2>Comfortable</h2>
+      <br />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="MySQL"/>
+        <SkillList src={checkMarkIcon} skill="C"/>
+        <SkillList src={checkMarkIcon} skill="jQuery"/>
+        <SkillList src={checkMarkIcon} skill="PHP"/>
         <SkillList src={checkMarkIcon} skill="Redux"/>
-        <SkillList src={checkMarkIcon} skill="Webpack"/>
-        <SkillList src={checkMarkIcon} skill="Git"/>
-        <SkillList src={checkMarkIcon} skill="Jest"/>
         <SkillList src={checkMarkIcon} skill="Bootstrap"/>
+        <SkillList src={checkMarkIcon} skill="Wordpress"/>
       </div>
       {/* <hr /> */}
     </section>
